38dd7cd81c6d665c5f6d371769f9eca0d7ce910c
Mohammad Rijwan <ridz.creation4@gmail.com>
2022-10-22 17:43:36 +0530

    feat(category): added animation

--- lib/src/core/ui/common_widget/category_card.dart
+++ lib/src/core/ui/common_widget/category_card.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:makeup/src/core/ui/common_widget/app_decoration.dart';
@@ -22,52 +23,54 @@ class CategoryCard extends StatelessWidget {
       onTap: () {
         onCardTap();
       },
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.r),
-            border: Border.all(color: AppColors.white, width: 1.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white24,
-                spreadRadius: 10.r,
-                blurRadius: 30.r,
+      child: FadeInLeft(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.r),
+              border: Border.all(color: AppColors.white, width: 1.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white24,
+                  spreadRadius: 10.r,
+                  blurRadius: 30.r,
+                ),
+              ],
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: Image.asset(
+                  imageUrl,
+                ).image,
               ),
-            ],
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: Image.asset(
-                imageUrl,
-              ).image,
             ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(6.r),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      label,
-                      style: AppDecoration.textStyle(
-                        color: AppColors.yellow,
-                        fontSize: fontSize ?? 20.sp,
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.italic,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(6.r),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        label,
+                        style: AppDecoration.textStyle(
+                          color: AppColors.yellow,
+                          fontSize: fontSize ?? 20.sp,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
